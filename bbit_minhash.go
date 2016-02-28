393a9a0c721d602c2c490fef54fcf662bec2bf2d
huydx <doxuanhuy@gmail.com>
2016-02-28 13:32:32 +0900

    bbit minhash reference by http://yukihir0.hatenablog.jp/entry/2013/08/15/142713

--- bbit_minhash.go
+++ bbit_minhash.go
@@ -1,43 +1,85 @@
 package bigg
 
 import (
-	"bytes"
+	"fmt"
 	"github.com/huydx/murmur3"
+	"math/rand"
+	"time"
 )
 
 type bbitMinHash struct {
-	bitnum  int
-	hashnum int
+	bitnum  uint64
+	hashnum uint64
 }
 
-func NewBBitMinHash(b int, h int) *bbitMinHash {
+func NewBBitMinHash(b uint64, h uint64) *bbitMinHash {
 	d := new(bbitMinHash)
 	d.bitnum = b
 	d.hashnum = h
 	return d
 }
 
-func (b *bbitMinHash) Hash(set []string, seed uint32) []byte {
-	hash := murmur3.New32(seed)
-	mask := (1 << b.bitnum) - 1
+func (b *bbitMinHash) MinHash(set []string, seed uint64) uint64 {
+	hash := murmur3.New64(seed)
+	minHash := ^uint64(0)
 
 	for _, item := range set {
 		hash.Write([]byte(item))
-		s := hash.Sum32()
+		s := hash.Sum64()
 		if s < minHash {
 			minHash = s
 		}
 	}
 
-	return []byte(minHash & mask)
+	return minHash
 }
 
-func (b *bbitMinHash) Jaccard(set1 []string, set2 []string) float32 {
-	correct := 0
+// Straight and simple C to Go translation from https://en.wikipedia.org/wiki/Hamming_weight
+func popcountHD(x uint64) uint64 {
+	x -= (x >> 1) & 0x55555555
+	x = (x & 0x33333333) + ((x >> 2) & 0x33333333)
+	x = (x + (x >> 4)) & 0x0f0f0f0f
+	x = x + (x >> 8)
+	x = x + (x >> 16)
+	return uint64(x & 0x0000003f)
+}
+
+func (b *bbitMinHash) Signature(set []string) uint64 {
 	rand.Seed(time.Now().UnixNano())
+	sig := uint64(0)
+	mask := uint64(0x1)
 
-	//http://research.microsoft.com/pubs/120078/wfc0398-lips.pdf
-	//using above paper to estimate jaccard
+	for i := uint64(0); i < b.hashnum; i++ {
+		rand_seed := uint64(rand.Intn(99999))
+		mhash := b.MinHash(set, rand_seed)
 
-	return 0
+		sig += (mhash & mask) << i
+	}
+
+	return sig
+}
+
+func printAsBitArray(in uint64) {
+	for in > 0 {
+		if in&1 == 1 {
+			fmt.Print(1)
+		} else {
+			fmt.Print(0)
+		}
+		in = in >> 1
+	}
+}
+
+func (b *bbitMinHash) Jaccard(set1 []string, set2 []string) float64 {
+	s1 := b.Signature(set1)
+	s2 := b.Signature(set2)
+
+	printAsBitArray(s1)
+	fmt.Println()
+	printAsBitArray(s2)
+	fmt.Println()
+
+	common := s1 ^ s2
+
+	return 2.0 * ((float64(b.hashnum)-float64(popcountHD(common)))/float64(b.hashnum) - 0.5)
 }
--- bbit_minhash_test.go
+++ bbit_minhash_test.go
@@ -1,9 +1,22 @@
 package bigg
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestRefBBitMinHash(t *testing.T) {
+func TestPopCountHD(t *testing.T) {
+	x := uint64(1)
+	y := uint64(7)
+	z := uint64(8)
+	if popcountHD(x) != 1 || popcountHD(y) != 3 || popcountHD(z) != 1 {
+		t.Errorf("pop count mismatch")
+	}
+}
 
+func TestRefBBitMinHash(t *testing.T) {
+	m := NewBBitMinHash(1, 128)
+	set1 := []string{"ff", "asda"}
+	set2 := []string{"ff"}
+	fmt.Println(m.Jaccard(set1, set2))
 }
